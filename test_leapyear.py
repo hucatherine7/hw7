518263a6c48c4bd56c774b4697a606270095209c
hucatherine7 <hucatherine7@gmail.com>
2021-03-07 21:38:07 -0800

    Wrote final test for input 300, should return False, input divisiable by 100 and 4 but not 400

--- test_leapyear.py
+++ test_leapyear.py
@@ -13,5 +13,8 @@ class TestLeapYear(unittest.TestCase):
     def test_leapyear(self):
         self.assertEqual(leapyear.lpyear(400), True)
 
+    def test_leapyear(self):
+        self.assertEqual(leapyear.lpyear(300), False)
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
