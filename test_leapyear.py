99bcf2b7d2218c589e32975cc33158ac69502248
hucatherine7 <hucatherine7@gmail.com>
2021-03-07 21:35:04 -0800

    Wrote test for input 400, value should be True

--- test_leapyear.py
+++ test_leapyear.py
@@ -10,5 +10,8 @@ class TestLeapYear(unittest.TestCase):
     def test_leapyear(self):
         self.assertEqual(leapyear.lpyear(12), True)
 
+    def test_leapyear(self):
+        self.assertEqual(leapyear.lpyear(400), True)
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
