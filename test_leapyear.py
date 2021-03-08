3f8d738fbd3d8d83903f256211d2eeebf6dae839
hucatherine7 <hucatherine7@gmail.com>
2021-03-07 21:32:24 -0800

    Wrote test for input not divisible by 100 but divisible by 4

--- test_leapyear.py
+++ test_leapyear.py
@@ -6,3 +6,9 @@ import leapyear
 class TestLeapYear(unittest.TestCase):
     def test_leapyear(self):
         self.assertEqual(leapyear.lpyear(7), False)
+
+    def test_leapyear(self):
+        self.assertEqual(leapyear.lpyear(12), True)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
