c7a03cb374d99382a8f369003d4767665468b75a
hucatherine7 <hucatherine7@gmail.com>
2021-03-07 20:50:40 -0800

    Wrote code for implementing any other number return themself, input 99

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,9 +6,11 @@ def fizzbuzz(num):
 
     if num % 3 == 0 and num % 5 == 0:
         return 'FizzBuzz'
-        
+
     if num % 3 == 0:
         return 'Fizz'
 
     if num % 5 == 0:
         return 'Buzz'
+
+    return num
