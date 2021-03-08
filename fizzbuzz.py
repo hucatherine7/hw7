28d47038ee07bb424695a7724d94f551a6308342
hucatherine7 <hucatherine7@gmail.com>
2021-03-07 20:46:47 -0800

    Wrote code for implementing input 15 will print out FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,6 +3,10 @@
 #Just enough code for the test to pass
 
 def fizzbuzz(num):
+
+    if num % 3 == 0 and num % 5 == 0:
+        return 'FizzBuzz'
+        
     if num % 3 == 0:
         return 'Fizz'
 
