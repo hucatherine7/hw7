864d1352270d687488e0474ba4aa623a238af581
hucatherine7 <hucatherine7@gmail.com>
2021-03-07 20:38:27 -0800

    Wrote test for input 5 equals buzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -10,6 +10,9 @@ class TestFizzbuzz(unittest.TestCase):
     def test_fizzbuzz(self):
         self.assertEqual(fizzbuzz.fizzbuzz(3), 'Fizz')
 
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz.fizzbuzz(5), 'Buzz')
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
